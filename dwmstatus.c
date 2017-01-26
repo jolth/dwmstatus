c63adf3fc37f9761f7c5b9d59c5355af518a9a17
jolth <jolth@debian.local>
2017-01-26 16:26:06 -0500

    update

--- dwmstatus.c
+++ dwmstatus.c
@@ -268,15 +268,6 @@ void getcore(char cores[4][5]){
 #define WHT   "\x1B[37m"
 #define RESET "\x1B[0m"
 
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
-
 int main(void) {
 	char *status;
 	char *avgs;
